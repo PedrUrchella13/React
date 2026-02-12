0d39331f584d2a2ebf2d616530c4745feb665028
PedrUrchella13 <pedrourchella@gmail.com>
2026-02-12 10:07:21 -0300

    fix: update vite version to ^7.3.1 and enhance vite configuration

--- 02-react-vite/vite.config.js
+++ 02-react-vite/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, //Muda a porta padrão Vite para padrão react
+    open: true, //Abre o navegador automaticamente
+    host: true,
+  }, //Permite que tenha acesso do seu IP na rede local
+});
