b7ac508789511c73d66185f628747dc51fca6bcc
PedrUrchella13 <pedrourchella@gmail.com>
2026-05-08 11:29:04 -0300

    continuidade ao projeto com metade da promocao feita

--- 07-devsteam/src/App.jsx
+++ 07-devsteam/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+
 import Header from './components/Header/Header'
+import Promocao from './components/Promocao/Promocao'
 
 function App() {
   return (
     <>
       <div id="Body">
-        <Header></Header>
+        <Header>/</Header>
+        <div><Promocao/></div>
+       
       </div>
     </>
   )
--- 07-devsteam/src/components/Promocao/Promocao.jsx
+++ 07-devsteam/src/components/Promocao/Promocao.jsx
@@ -1,11 +1,32 @@
-import Styles from './Promocao.module.css'
+import styles from "./Promocao.module.css";
 
 const Promocao = () => {
   return (
-    <div>
-      
+    <div id={styles.card}>
+      <div id={styles.top_banner}>
+        <img
+          className={styles.image}
+          src="https://placehold.co/250x300"
+          alt="Imagem de jogo em promoção"
+        />
+      </div>
+      <div id={styles.bottom_info}>
+        <div className={styles.offer}>
+          <h1>OFERTA EXCLUSIVA</h1>
+        </div>
+        <div className={styles.discount}>
+          <div className={styles.discountPercentage}>
+            <h1>-50%</h1>
+          </div>
+          <div className={styles.discountPrice}>
+            <h3>R$199,80</h3>
+            <h1></h1>
+          </div>
+        </div>
+        <div className={styles.addCart}></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Promocao
+export default Promocao;
